b58f522ddcf1a4fcfd0b436439aa00402d4cb2a6
lucas-lima-dev <lucas.sdslima@gmail.com>
2023-02-25 21:27:57 -0300

    Fix: Deploy Render try2

--- src/configs/database.js
+++ src/configs/database.js
@@ -5,19 +5,22 @@ dotenv.config()
 
 const {Pool} = pg;
 
+// const configDatabase = {
+//     connectionString: process.env.DATABASE_URL,
+//     ...(process.env.NODE_ENV === "production" && {
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//     }),
+//   };
 const configDatabase = {
-    connectionString: process.env.DATABASE_URL,
-    ...(process.env.NODE_ENV === "production" && {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }),
-  };
+  connectionString: process.env.DATABASE_URL,
+};
 
-  // if(process.env.MODE = "prod"){
+  if(process.env.MODE = "prod"){
 
-  //   configDatabase.ssl = true
-  // }
+    configDatabase.ssl = true
+  }
   
   const db = new Pool(configDatabase);
   
